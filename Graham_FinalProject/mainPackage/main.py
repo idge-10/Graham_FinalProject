5b8d426a5de54356e56ad6f9b4984c81342ee24d
123is <123is@DESKTOP-H4C32BG>
2022-11-22 08:45:34 -0500

    Initial Commit

---
+++ Graham_FinalProject/mainPackage/main.py
@@ -0,0 +1,22 @@
+"""
+Name: Isaac Hedges, Ben Truax 
+email: hedgesic@mail.uc.edu, truaxbp@mail.uc.edu
+Assignment: Assignment11
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: Writing using an API to find unique information
+Citations:
+Anything else that's relevant: 
+"""
+
+import json  #built in, no pip required (if needed, uses "jsons" import)
+import requests
+
+response = requests.get('https://api.wheretheiss.at/v1/satellites/25544')
+json_string = response.content
+
+# Parse the results
+parsed_json = json.loads(json_string) # Now we have a python dictionary
+
+# Check on parsed results
+print(parsed_json)
