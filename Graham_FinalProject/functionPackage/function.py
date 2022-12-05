7ae5096534dfcfc0c9de7c8566bd275eb0a44a86
truax <truax@DESKTOP-M6FLMOK.wifi.uc.edu>
2022-12-05 17:16:37 -0500

    Simplified it to 2 function and commented out the rest

--- Graham_FinalProject/functionPackage/function.py
+++ Graham_FinalProject/functionPackage/function.py
@@ -4,17 +4,38 @@ Created on Nov 22, 2022
 @author: 123is
 '''
 import json
-from PIL import Image, ImageFilter, ImageDraw, ImageFont
-import os, sys
-# import requests
-#from io import BytesIO
+from PIL import Image
 
-def file_path_Encrypt():
-    Encrypt = "EncryptedGroupHints.json"
+def encrypted(Encrypt,English,Code):
     with open(file=Encrypt, mode='r') as read_file:
         object = json.load(read_file)
-        print(object['Graham'])
+        Coordiates = object[Code]
+        print(Coordiates)
         
+        English = open(English, "r")
+        data = English.read()
+        location = ''
+        
+        
+def load_image( filename ) :
+    try:
+        myimage = Image.open(filename)
+        myimage.load()
+        return myimage
+    except:
+        return None
+
+'''
+def file_path_Encrypt():
+    hints = "EncryptedGroupHints.json"
+    with open(file=Encrypt, mode='r') as read_file:
+        object = json.load(read_file)
+        Graham = (object['Graham'])
+        output_json = json.loads(Graham)
+    for i in output_json:
+        print (i)
+        for k in output_json[i]:
+            print (k)
 # maybe something like this to parse the data to pair the encrypted data with the
 # text file?
 def parseJsonData(Graham):
@@ -29,12 +50,4 @@ def file_path_English():
     file_contents2 = English.read()
     print (file_contents2)
     English.close()
-def load_image( filename ) :
-    try:
-        myimage = Image.open(filename)
-        myimage.load()
-        return myimage
-    except:
-        return None
-   
-    
+   ''' 
--- Graham_FinalProject/mainPackage/main.py
+++ Graham_FinalProject/mainPackage/main.py
@@ -1,4 +1,4 @@
-"""
+"""a
 Name: Isaac Hedges, Ben Truax, Jack Hutz
 email: hedgesic@mail.uc.edu, truaxbp@mail.uc.edu
 Assignment: Assignment12
@@ -8,36 +8,11 @@ Brief Description:
 Citations:
 Anything else that's relevant: 
 """
+
+
+
 from functionPackage.function import *
-import json  #built in, no pip required (if needed, uses "jsons" import)
-import requests
-from functionPackage.function import file_path_English
-from PIL import Image, ImageFilter, ImageDraw, ImageFont
-import os, sys
-# import requests
-from io import BytesIO
-# This one is in Main.py only
-from image_functions import *
-# file_path_English()
 
-# file_path_Encrypt()
-my_image = load_image("bridge.jpg")
-my_image.show(my_image)
-    
-
-
-'''
-# find the files
-file_path_English = "EncryptedGroupHints.json"
-file_path_Encrypted = "EncryptedGroupHints.json"
-
-# open the files
-with open(file=file_path_Encrypted, mode='r') as read_file:
-    object = json.load(read_file)
-with open(file=file_path_Encrypted, mode='r') as read_file:
-    object = json.load(read_file)
-    print(object['Graham'])
-    
-if object['Graham']:
- '''   
-    
+encrypted('EncryptedGroupHints.json','english.txt','Graham')
+# my_image = load_image("SiriusAndViolet.jpg")
+# my_image.show(my_image)
