250c06132fbb7dad0605f21ef633f4928f97a0d7
123is <123is@DESKTOP-H4C32BG.wifi.uc.edu>
2022-12-05 20:29:37 -0500

    Final commit

--- Graham_FinalProject/functionPackage/function.py
+++ Graham_FinalProject/functionPackage/function.py
@@ -1,66 +1,42 @@
-'''
-Created on Nov 22, 2022
-
-@author: 123is
-'''
+"""
+Name: Isaac Hedges, Ben Truax, Jack Hutz, Alyssa Hughes
+email: hedgesic@mail.uc.edu, truaxbp@mail.uc.edu, hutzjm@mail.uc.edu, hugheah@mail.uc.edu
+Assignment: Final Project
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: Decrypting txt and json file and uploading image
+Citations: https://www.google.com/search?q=what+is+split+in+python&oq=what+is+split+in+&aqs=chrome.0.0i512l10.2651j0j7&sourceid=chrome&ie=UTF-8
+Anything else that's relevant: 
+"""
 import json
 from PIL import Image
 
+
+# creating a function to decrypt
 def encrypted(Encrypt,English,Code):
+    # reading the EncryptedGroupHints.json file
     with open(file=Encrypt, mode='r') as read_file:
-        object = json.load(read_file)
-        Coordiates = object[Code]
-        print(Coordiates)
-        
+        File = json.load(read_file)
+        Coordinates = File[Code]
+    # reading the english.txt file   
         English = open(English, "r")
         data = English.read()
-        data_into_list = data.split("\n")
-        
-        for num in Coordiates:
-            location = (data_into_list[int(num)])
-            print(location)
-        
-        temp = set(English)
-        res = [i for i, val in enumerate() if val in temp]  
-        
-        
-        
-        
-        
+    # using split to turn string into list
+        dataSet = data.split()
+    # parsing files to find the decrypted location   
+        location = ""
+        for num in Coordinates:
+            location += (dataSet[int(num)]) + " "
+        print(location)
+ # creating function to load the image taken at decrypted location               
 def load_image( filename ) :
     try:
+        # opening image
         myimage = Image.open(filename)
-        myimage.load()
-        return myimage
+        # rotating image
+        rotate_image = myimage.rotate(270)
+        # showing image
+        rotate_image.show(rotate_image)
     except:
         return None
     
-
- 
-
-'''
-def file_path_Encrypt():
-    hints = "EncryptedGroupHints.json"
-    with open(file=Encrypt, mode='r') as read_file:
-        object = json.load(read_file)
-        Graham = (object['Graham'])
-        output_json = json.loads(Graham)
-    for i in output_json:
-        print (i)
-        for k in output_json[i]:
-            print (k)
-# maybe something like this to parse the data to pair the encrypted data with the
-# text file?
-def parseJsonData(Graham):
-    output_json = json.loads(Graham)
-    for i in output_json:
-        print (i)
-        for k in output_json[i]:
-            print (k)
-
-def file_path_English():
-    English = open("English.txt")
-    file_contents2 = English.read()
-    print (file_contents2)
-    English.close()
-   ''' 
--- Graham_FinalProject/mainPackage/main.py
+++ Graham_FinalProject/mainPackage/main.py
@@ -1,18 +1,20 @@
-"""a
-Name: Isaac Hedges, Ben Truax, Jack Hutz
-email: hedgesic@mail.uc.edu, truaxbp@mail.uc.edu
-Assignment: Assignment12
+"""
+Name: Isaac Hedges, Ben Truax, Jack Hutz, Alyssa Hughes
+email: hedgesic@mail.uc.edu, truaxbp@mail.uc.edu, hutzjm@mail.uc.edu, hugheah@mail.uc.edu
+Assignment: Final Project
 Course: IS 4010
 Semester/Year: Fall 2022
-Brief Description: 
-Citations:
+Brief Description: Decrypting txt and json file and uploading image
+Citations: https://www.google.com/search?q=what+is+split+in+python&oq=what+is+split+in+&aqs=chrome.0.0i512l10.2651j0j7&sourceid=chrome&ie=UTF-8
 Anything else that's relevant: 
 """
 
 
 
 from functionPackage.function import *
-
+# printing statement for location
 Code = encrypted('EncryptedGroupHints.json','english.txt','Graham')
-# my_image = load_image("SiriusAndViolet.jpg")
-# my_image.show(my_image)
+# showing the image taken at location
+my_image = load_image("Team_Graham_Picture.jpg")
+
+
